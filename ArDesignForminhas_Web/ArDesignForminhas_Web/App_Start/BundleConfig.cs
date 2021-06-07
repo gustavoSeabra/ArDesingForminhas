37babc91ab0e275420c71abc48b18fe5890b8422
gustavo_seabra <gustavo.henrique.ss@gmail.com>
2021-06-07 00:17:03 -0300

    Adicionando plugin FileUpload

--- ArDesignForminhas_Web/ArDesignForminhas_Web/App_Start/BundleConfig.cs
+++ ArDesignForminhas_Web/ArDesignForminhas_Web/App_Start/BundleConfig.cs
@@ -26,6 +26,18 @@ namespace ArDesignForminhas_Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryMask").Include(
                         "~/Scripts/jquery.mask.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/fileinput").Include(
+                        "~/Scripts/fileinput.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/fasTheme").Include(
+                        "~/Content/bootstrap-fileinput/themes/fas/theme.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/fasThemefileinput").Include(
+                        "~/Content/bootstrap-fileinput/themes/explorer-fas/theme.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/fileInputPt-BR").Include(
+                "~/Scripts/locales/pt-BR.js"));
+
             // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender. Em seguida, quando estiver
             // pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -38,7 +50,9 @@ namespace ArDesignForminhas_Web
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
                       "~/Content/DataTable.css",
-                      "~/Content/sweetalert.css"));
+                      "~/Content/sweetalert.css",
+                      "~/Content/bootstrap-fileinput/css/fileinput.min.css",
+                      "~/Content/bootstrap-fileinput/themes/explorer-fa/theme.min.css"));
         }
     }
 }
