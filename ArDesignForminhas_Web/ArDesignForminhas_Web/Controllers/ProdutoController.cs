72e97347ac43b97f080759b954937cf0d553b60f
Gustavo Seabra <gustavo.henrique.ss@gmail.com>
2021-08-11 21:32:33 -0300

    Correção de bug Criação da tela de listagem de produtos por categoria Inicio da tela de detalhe do produto

---
+++ ArDesignForminhas_Web/ArDesignForminhas_Web/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArDesignForminhas_Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult detalhes(int id)
+        {
+            return View();
+        }
+    }
+}
